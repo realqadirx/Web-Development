5e3788f693789e9b2ca3d519c27d28ab427434af
Qadir <realqadirx@gmail.com>
2026-05-19 12:05:00 +0530

    Practicing the Assignment

---
+++ JavaScript/part3/Assi4.js
@@ -0,0 +1,61 @@
+// Answer number 1.
+// let arr = [1,2,3,4,5,6,2,3];
+// let num = 2;
+
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i] == num){
+//         arr.splice(i, 1);
+//     }
+// }
+
+// console.log(arr);
+
+// Answer number 2.
+// let num = 8280;
+// let count = 0;
+ 
+// let copy = num;
+
+// while(copy > 0) {
+//     count++;
+//     copy = Math.floor(copy/10);
+// }
+// console.log(count);
+
+
+// Answer number 3.
+// let num = 287152;
+// let sum = 0;
+
+// copy = num;
+
+// while(copy > 0){
+//     let digit = copy % 10;
+//     sum += digit;
+//     copy = Math.floor(copy/10);
+// }
+
+// console.log(sum);
+
+// Answer number 4.
+
+// let n = 7;
+// let factorial = 1;
+
+// for(let i=1; i<=n; i++){
+//     factorial *= i;
+// }
+// console.log(`Factorial of ${n} is ${factorial}`);
+
+
+// Answer number 5.
+// let num = [1,2,3,4,5,6,2,3];
+// let largest = num[0];
+
+// for(let i=1; i<num.length; i++){
+//     if(num[i] > largest){
+//         largest = num[i];
+//     }
+// }
+
+// console.log(`Largest number is : ${largest}`);
