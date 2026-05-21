7a51a529a11ce221b261fc8e2e3ce16547b1af94
Qadir <realqadirx@gmail.com>
2026-05-21 12:05:00 +0530

    Practicing the assignment-5

---
+++ JavaScript/part4/assi5.js
@@ -0,0 +1,32 @@
+// Ques-1 Create a program that generates a random number representing a dice roll.[The number should be between 1 and 6].
+// let random = Math.floor(Math.random() * 6) + 1;
+// console.log(random);
+
+
+// Qs2.Create an object representing a car that stores the following properties for the car: name, model, color. Print the car’s name.
+// const car = {
+
+//         name: "BMW",
+//         model: "M4-Competition",
+//         color: "Black",
+//         speed: "350km"
+
+// };
+
+// console.log(car.name);
+
+
+// Ques-3 Create an object Person with their name, age and city.Edit their city’s original value to change it to “NewYork”.Add a new property country and set it to the United States.
+
+// const person = [
+//     Info = {
+//         name: "kritika",
+//         age: 21,
+//         city: "New delhi",
+//     },
+// ];
+
+// Info.city = "New york";
+// Info.country = "United states";
+
+// console.log(Info);
