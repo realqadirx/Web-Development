8d45be26e27ef56f8aabe8fc0d26587b47446f5d
Qadir <realqadirx@gmail.com>
2026-05-23 12:05:00 +0530

    Learing the return function

---
+++ JavaScript/part5/Return.js
@@ -0,0 +1,33 @@
+// function sum(a, b){
+//     return a+b;
+// }
+
+// console.log(sum(4,3)); 
+
+
+// Create a function that returns the sum of numbers from 1 to n.
+
+// function getSum(n){
+//     let sum = 0;
+
+//     for(let i=1; i<=n; i++){
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+
+
+// Create a function that returns the concatenation of all strings in an array.
+
+
+// let str = ["hello", "my", "name", "is", "qadir"];
+// function conArr(str) {
+//     let result = "";
+//     for(let i=0; i<str.length; i++){
+//         result += str[i];
+//     }
+//     return result;
+// }
+
+
