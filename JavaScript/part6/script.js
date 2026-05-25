dd0c64df444986a1f9d6d42091d60d317ffb6e6d
Qadir <realqadirx@gmail.com>
2026-05-25 12:05:00 +0530

    Learing the miscellaneous topics

---
+++ JavaScript/part6/script.js
@@ -0,0 +1,36 @@
+// "this" Keyword.
+
+// let student = {
+//     name: "Qadir",
+//     age: 21,
+//     eng: 95,
+//     math: 93,
+//     phy: 97,
+//     getAvg() {
+//         console.log(this);
+//         let avg = (this.eng + this.math + this.phy) / 3;
+//         console.log(`${this.name} got avg marks = ${avg}`);
+//     }
+// }
+
+
+// function getAvg(){
+//     console.log(this);
+// } 
+
+
+
+// try and catch.
+
+// console.log("hello");
+// console.log("hello");
+// // let a = 5;
+// try {
+//     console.log(a);
+// } catch(err) {
+//     console.log("caugth an error... a is not defined");
+//     console.log(err);
+// }
+// console.log("hello2");
+// console.log("hello2");
+// console.log("hello2");
