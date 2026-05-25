5e17747f25438e18d11c14e46cc25b02b8da22e6
Qadir <realqadirx@gmail.com>
2026-05-25 12:05:00 +0530

    Learing the miscellaneous topics

---
+++ JavaScript/part6/Miscellaneous.js
@@ -0,0 +1,70 @@
+// Arrow Function.
+
+// const sum = (a, b) => (a + b); // Implicit return.
+
+
+// Area of a cylinder by using Arrow function.
+// const cylinder = (radius, height) => {
+//     return (Math.PI * (radius * radius) * height);
+// };
+
+// console.log(cylinder(5, 10));
+
+
+// Area of a triangle by using Arrow Function.
+
+// let half = 0.5;
+// const triangle = (half, base, height) => {
+//     return half * base * height;
+// };
+
+// console.log(triangle(half, 18, 5));
+
+
+// For finding cube by using arrow function.
+
+// const cube = (n) => {
+//     return n*n*n;
+// } ;
+
+// const power = (a, b) => {
+//     return a**b;
+// }
+
+
+// Implicit return.
+
+// const mul = (a, b) => (a * b); // Implicit return.
+
+
+// settimeout Function.
+
+
+// console.log("hi there!");
+// setTimeout( () => {
+//     console.log("QadirKritika")
+// }, 4000);
+
+// console.log("welcome to");  
+
+
+// set Interval.
+
+// console.log("hi there");
+
+// let id = setInterval(() => {
+//     console.log("Qadir");
+// }, 2000);
+
+// console.log(id);
+// clearInterval(id);
+
+
+
+// let id2 = setInterval(() => {
+//     console.log("Hello world");
+// }, 2000);
+
+// console.log(id2);    
+// clearInterval(id2);
+
