e702d18f797977a19efc075f0abb6f21e87dc01a
Qadir <realqadirx@gmail.com>
2026-05-27 12:05:00 +0530

    creating a demo user

---
+++ JavaScript/part6/demo.js
@@ -0,0 +1,68 @@
+// function Hello() {
+//     console.log("Hello my name is Qadir!");
+// }
+
+
+// function print1to5() {
+//     for (let i = 1; i <= 5; i++) {
+//         console.log(i);
+//     }
+// }
+
+// function isAdult(age) {
+//     if (age >= 18) {
+//         console.log("You can vote");
+//     } else {
+//         console.log("you can not vote");
+//     }
+// }
+
+
+
+// function poem() {
+//     console.log("Twinkle, twinkle, little star");
+//     console.log("how I wonder what you are");
+//     console.log("Up above the world so high like a diamond in the sky.");
+//     console.log("Twinkle, twinkle, little star how I wonder what you are");
+// }
+
+
+
+// function rollDice() {
+//     let rand = Math.floor(Math.random() * 6) + 1;
+//     console.log(rand);
+// }
+
+
+// function funcName(name, age, city) {
+//     console.log(`${name}'s age is ${age}`);
+// }
+
+// funcName("Kritika", 21);
+// funcName("Qadir", 20);
+
+
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+
+// sum(10, 5);
+
+
+// function avg(a, b, c) {
+//     let avg = (a + b + c) / 3;
+//     console.log(avg);
+// }
+
+// avg(20, 20, 25);
+
+
+// function crateTable(n) {
+//     for (let i = n; i <= n * 10; i += n) {
+//         console.log(i);
+//     }
+// }
+
+let Name = "Qadir";
+console.log(`Hello ${Name}, welcome!`);
