1fb783a0ed37fc8f115edc13eb5a9aa0fd567035
Qadir <realqadirx@gmail.com>
2026-05-28 12:05:00 +0530

    creating Our first API

---
+++ JavaScript/part12/API.js
@@ -0,0 +1,45 @@
+let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+
+
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log(data2.fact);
+    } catch (err) {
+        console.log("Error - ", err);
+    }
+    console.log("Good bye");
+}
+
+
+
+
+
+
+
+
+// fetch(url)
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((data) => {
+//         console.log("data1 = ", data.fact);
+//         return fetch(url);
+//     })
+
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((data2) => {
+//         console.log("data2 = ", data2.fact);
+//     })
+//     .catch((err) => {
+//         console.log("ERROR -", err);
+//     });
+
+
